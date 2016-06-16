45e304295056e4b85e7af8d8cb55e73bee58b439
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-06-16 12:55:34 +0300

    New motion_analysis communication

--- src/consumer.py
+++ src/consumer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import roslib, rospy
-from motion_analysis.msg import StringWithHeader
+from motion_analysis.msg import AnswerWithHeader
 
 topic = ''
 
@@ -9,13 +9,20 @@ def init():
     rospy.init_node('motion_analysis_consumer')
     print "Do not forget to assign the correct image topic for motion analysis!"
     topic = rospy.get_param("~topic", "/motion_analysis/event/human_transfer")
-    rospy.Subscriber(topic, StringWithHeader, stringCallback)
+    rospy.Subscriber(topic, AnswerWithHeader, stringCallback)
     print "Subscribed to",topic,"..."
     while not rospy.is_shutdown():  
         rospy.spin()
 
 def stringCallback(msg):
-    print msg.event
+    print 'Real message value = ',msg.event
+    if msg.event == 0:
+        print 'Sitting on bed'
+    elif msg.event == 1:
+        print 'Stood up'
+    elif msg.event == 2:
+        print 'Walking'
+
 
 
 if __name__ == '__main__':
